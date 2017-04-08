6f92f11e3bab6169b5ec097920ab295bae13fb72
praveendareddy21 <praveendareddy@gmail.com>
2017-04-07 23:00:13 -0700

    added header files for roubust io functions

--- robust_socket_io/fancyRW.h
+++ robust_socket_io/fancyRW.h
@@ -1,10 +1,19 @@
 /*
- * write template functions that are guaranteed to read and write the 
+ * write template functions that are guaranteed to read and write the
  * number of bytes desired
  */
 
 #ifndef fancyRW_h
 #define fancyRW_h
+
+#include <sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h> //for read/write
+#include<netdb.h>
+#include<string.h> //for memset
+#include<stdio.h> //for fprintf, stderr, etc.
+#include<stdlib.h> //for exit
+#include<errno.h>
 
 template<typename T>
 int READ(int fd, T* obj_ptr, int count)
@@ -24,6 +33,8 @@ int WRITE(int fd, T* obj_ptr, int count)
 //Example of a method for testing your functions shown here:
 //opened/connected/etc to a socket with fd=7
 //
+/*
+
 int count=write(7, "Todd Gibson", 11);
 cout << count << endl;//will display a number between 1 and 11
 int count=write(7, "d Gibson", 8);
@@ -33,5 +44,8 @@ write(7, "To", 2);
 write(7, "DD ", 3);
 write(7, "Gibso", 5);
 write(7, "n", 1);
+
+
+*/
 //
 //
