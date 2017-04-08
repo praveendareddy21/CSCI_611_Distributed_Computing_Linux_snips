8c49de98e5b33b907bb39eb756da9fdf818e397c
praveendareddy21 <praveendareddy@gmail.com>
2017-04-07 22:49:54 -0700

    added non-robust server and client with Makefile

---
+++ robust_socket_io/client.cpp
@@ -0,0 +1,54 @@
+//Socket client
+#include <sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h> //for read/write
+#include<netdb.h>
+#include<string.h> //for memset
+#include<stdio.h> //for fprintf, stderr, etc.
+#include<stdlib.h> //for exit
+
+int main()
+{
+  int sockfd; //file descriptor for the socket
+  int status; //for error checking
+
+  //change this # between 2000-65k before using
+  const char* portno="42424";
+
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints)); //zero out everything in structure
+  hints.ai_family = AF_UNSPEC; //don't care. Either IPv4 or IPv6
+  hints.ai_socktype=SOCK_STREAM; // TCP stream sockets
+
+  struct addrinfo *servinfo;
+  //instead of "localhost", it could by any domain name
+  if((status=getaddrinfo("localhost", portno, &hints, &servinfo))==-1)
+  {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+  }
+  sockfd=socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
+  if((status=connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen))==-1)
+  {
+    perror("connect");
+    exit(1);
+  }
+  //release the information allocated by getaddrinfo()
+  freeaddrinfo(servinfo);
+
+  const char* message="One small step for (a) man, one large  leap for Mankind";
+  int n;
+  if((n=write(sockfd, message, strlen(message)))==-1)
+  {
+    perror("write");
+    exit(1);
+  }
+  printf("client wrote %d characters\n", n);
+  char buffer[100];
+  memset(buffer, 0, 100);
+  read(sockfd, buffer, 99);
+  printf("%s\n", buffer);
+  close(sockfd);
+  return 0;
+}
---
+++ robust_socket_io/fancyRW.h
@@ -0,0 +1,37 @@
+/*
+ * write template functions that are guaranteed to read and write the 
+ * number of bytes desired
+ */
+
+#ifndef fancyRW_h
+#define fancyRW_h
+
+template<typename T>
+int READ(int fd, T* obj_ptr, int count)
+{
+  char* addr=(char*)obj_ptr;
+  //loop. Read repeatedly until count bytes are read in
+}
+
+template<typename T>
+int WRITE(int fd, T* obj_ptr, int count)
+{
+  char* addr=(char*)obj_ptr;
+  //loop. Write repeatedly until count bytes are written out
+}
+#endif
+
+//Example of a method for testing your functions shown here:
+//opened/connected/etc to a socket with fd=7
+//
+int count=write(7, "Todd Gibson", 11);
+cout << count << endl;//will display a number between 1 and 11
+int count=write(7, "d Gibson", 8);
+//
+//How to test your READ template. Read will be: READ(7, ptr, 11);
+write(7, "To", 2);
+write(7, "DD ", 3);
+write(7, "Gibso", 5);
+write(7, "n", 1);
+//
+//
