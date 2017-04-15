f2c5d4cd66acf3e6e1b0bc89e2974def903bb2c2
praveendareddy21 <praveendareddy@gmail.com>
2017-04-15 01:48:51 -0700

    added server client duo for security protocol communication

---
+++ robust_socket_io_protocol/client.cpp
@@ -0,0 +1,104 @@
+//Socket client
+#include <sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h> //for read/write
+#include<netdb.h>
+#include<string.h> //for memset
+#include<stdio.h> //for fprintf, stderr, etc.
+#include<stdlib.h> //for exit
+#include<errno.h>
+#include"fancyRW.h"
+#include"goldchase.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int write_fd = -1;
+int read_fd = -1;
+
+
+int get_Write_Socket_fd(){
+  int sockfd; //file descriptor for the socket
+  int status; //for error checking
+
+  //change this # between 2000-65k before using
+  const char* portno="42424";
+
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints)); //zero out everything in structure
+  hints.ai_family = AF_UNSPEC; //don't care. Either IPv4 or IPv6
+  hints.ai_socktype=SOCK_STREAM; // TCP stream sockets
+
+  struct addrinfo *servinfo;
+  //instead of "localhost", it could by any domain name
+  if((status=getaddrinfo("localhost", portno, &hints, &servinfo))==-1)
+  {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+  }
+  sockfd=socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
+  if((status=connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen))==-1)
+  {
+    perror("connect");
+    exit(1);
+  }
+  //release the information allocated by getaddrinfo()
+  freeaddrinfo(servinfo);
+
+  printf("Connected to server.\n");
+  return sockfd;
+
+}
+void send_Socket_Message(char PLR_MASK, string msg);
+void send_Socket_Player(char PLR_MASK);
+void send_Socket_Map(char PLR_MASK, string msg);
+
+
+void send_Socket_Player(char PLR_MASK){
+
+  char protocol_type = G_SOCKPLR;
+  protocol_type &= PLR_MASK;
+
+  WRITE <char>(write_fd, &protocol_type, sizeof(char));
+
+
+}
+
+void send_Socket_Message(char PLR_MASK, string msg){
+  int msg_length = 0;
+  char protocol_type = G_SOCKMSG;
+  protocol_type &= PLR_MASK;
+
+  const char * msg_cstring = msg.c_str();
+  msg_length = msg.size();
+  printf("msglen %d - msg - %s\n",msg_length, msg_cstring);
+
+  WRITE <char>(write_fd, &protocol_type, sizeof(char));
+
+
+
+
+}
+
+int main()
+{
+  int write_fd = get_Write_Socket_fd();
+
+  char protocol_type = ' ';
+
+  protocol_type = G_SOCKPLR;
+  send_Socket_Message(protocol_type, "Hello World!!");
+
+  return 1;
+
+  protocol_type = G_SOCKPLR;
+  send_Socket_Player(protocol_type);
+
+
+
+  printf("Exiting socket connection.\n");
+  close(write_fd);
+
+}
---
+++ robust_socket_io_protocol/server.cpp
@@ -0,0 +1,135 @@
+//Socket server
+#include <sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<unistd.h> //for read/write
+#include<string.h> //for memset
+#include<stdio.h> //for fprintf, stderr, etc.
+#include<stdlib.h> //for exit
+#include<errno.h>
+#include"fancyRW.h"
+#include"goldchase.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+int write_fd = -1;
+int read_fd = -1;
+
+
+void socket_Communication_Handler();
+void process_Socket_Message(char protocol_type);
+void process_Socket_Player(char protocol_type);
+void process_Socket_Map(char protocol_type);
+
+void process_Socket_Message(char protocol_type){
+
+}
+
+void process_Socket_Player(char protocol_type){
+
+}
+
+void process_Socket_Map(char protocol_type){
+
+}
+
+
+int get_Read_Socket_fd(){
+  int sockfd; //file descriptor for the socket
+ int status; //for error checking
+
+
+ //change this # between 2000-65k before using
+ const char* portno="42424";
+ struct addrinfo hints;
+ memset(&hints, 0, sizeof(hints)); //zero out everything in structure
+ hints.ai_family = AF_UNSPEC; //don't care. Either IPv4 or IPv6
+ hints.ai_socktype=SOCK_STREAM; // TCP stream sockets
+ hints.ai_flags=AI_PASSIVE; //file in the IP of the server for me
+
+ struct addrinfo *servinfo;
+ if((status=getaddrinfo(NULL, portno, &hints, &servinfo))==-1)
+ {
+   fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+   exit(1);
+ }
+ sockfd=socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
+ /*avoid "Address already in use" error*/
+ int yes=1;
+ if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))==-1)
+ {
+   perror("setsockopt");
+   exit(1);
+ }
+
+ //We need to "bind" the socket to the port number so that the kernel
+ //can match an incoming packet on a port to the proper process
+ if((status=bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen))==-1)
+ {
+   perror("bind");
+   exit(1);
+ }
+ //when done, release dynamically allocated memory
+ freeaddrinfo(servinfo);
+
+ if(listen(sockfd,1)==-1)
+ {
+   perror("listen");
+   exit(1);
+ }
+
+ printf("Blocking, waiting for client to connect\n");
+
+ struct sockaddr_in client_addr;
+ socklen_t clientSize=sizeof(client_addr);
+ int new_sockfd;
+ if((new_sockfd=accept(sockfd, (struct sockaddr*) &client_addr, &clientSize))==-1)
+ {
+   perror("accept");
+   exit(1);
+ }
+ printf("Connected to client.\n");
+ return new_sockfd;
+
+}
+
+void socket_Communication_Handler(){
+  char protocol_type;
+
+  printf("Attempted to read.\n");
+
+  READ <char>(read_fd, &protocol_type, sizeof(char));
+
+  if (protocol_type&G_SOCKPLR ){
+    printf("read protocol_type - Socket_Player from client.\n");
+
+
+  }
+  else if (protocol_type&G_SOCKMSG ){
+    printf("read protocol_type - Socket_Message from client.\n");
+
+  }
+  else if (protocol_type ){
+    printf("read protocol_type - Socket_Map from client.\n");
+
+  }
+
+
+  //}   //end of while
+
+
+
+}
+
+int main()
+{
+  read_fd = get_Read_Socket_fd();
+  socket_Communication_Handler();
+
+  printf("Exiting socket connection.\n");
+  close(read_fd);
+}
